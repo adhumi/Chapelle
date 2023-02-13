8ec8700bdfa74e0918e23000682f1046eb9d9c05
Adrien Humilière <adhumi@gmail.com>
2023-02-13 16:01:40 +0100

    Initial Commit

---
+++ Chapelle/ChapelleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChapelleApp.swift
+//  Chapelle
+//
+//  Created by Adrien Humilière on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChapelleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
